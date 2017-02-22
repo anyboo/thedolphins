6776ad9b2e652e2cafe3c9fa9c9f5dd35a07aa49
Kevin Yu <janyboo@gmail.com>
2017-02-22 16:32:51 +0800

    change the logo image

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/clock.png">
     <h1>{{ msg }}</h1>
     <el-button @click.native="startHacking">Let's do it</el-button>
   </div>
@@ -10,7 +10,7 @@
 export default {
   data () {
     return {
-      msg: 'Use Vue 2.0 Today!'
+      msg: 'Thedolphins is going!'
     }
   },
 
