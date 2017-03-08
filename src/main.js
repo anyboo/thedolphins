7b787e2d9a7697076fffeb37aca15d4411768cfd
linzuxin <linzuxin@hotmail.com>
2017-03-08 17:49:08 +0800

    提交web导航和菜单代码

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './app.vue';
+import router from './router';
+import VueI18n from 'vue-i18n';
+import Element from 'element-ui';
+import enLocale from 'element-ui/lib/locale/lang/en';
+import zhLocale from 'element-ui/lib/locale/lang/zh-CN';
+import 'element-ui/lib/theme-default/index.css';
+import './styles/index.css';
+//require('es6-promise').polyfill();
 
-Vue.use(ElementUI)
+Vue.use(VueI18n);
+Vue.use(Element);
+Vue.config.lang = 'zh-cn';
+//Vue.locale('zh-cn', zhLocale);
+//Vue.locale('en', enLocale);
 
-new Vue({
+new Vue({ // eslint-disable-line
   el: '#app',
+  router,
   render: h => h(App)
-})
+});
