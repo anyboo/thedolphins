bed7d4abfa7dabfe0ff42a0bc8073b4463a6c374
linzuxin <linzuxin@hotmail.com>
2017-03-31 20:41:02 +0800

    veux添加

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import Vue from 'vue';
 import VueResouse from 'vue-resource';
 import App from './app.vue';
 import router from './router';
+import store from './store';
 //import VueI18n from 'vue-i18n';
 import Element from 'element-ui';
-//bullstech 控件
 import BtCom from './components/src';
 
-//import enLocale from 'element-ui/lib/locale/lang/en';
-//import zhLocale from 'element-ui/lib/locale/lang/zh-CN';
 import 'element-ui/lib/theme-default/index.css';
 import './styles/index.css';
 import './styles/btindex.css';
@@ -24,8 +22,9 @@ Vue.config.lang = 'zh-cn';
 //Vue.locale('zh-cn', zhLocale);
 //Vue.locale('en', enLocale);
 
-new Vue({ // eslint-disable-line
+new Vue({ 
     el: '#app',
+    store,
     router,
     render: h => h(App)
 });
