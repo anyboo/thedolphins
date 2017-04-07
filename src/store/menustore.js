16c6c70b4d8e883c2ed421f48120d42125f27373
chenji <chenji@bullstech.cn>
2017-04-07 17:18:59 +0800

    doc

--- src/store/menustore.js
+++ src/store/menustore.js
@@ -1,66 +1,60 @@
 export default [{
     menuTitle: '桌面',
-    menuIcon: 'fa-envelope-o',
+    menuIcon: 'fa-windows',
     to: '/desktop'
 }, {
     menuTitle: '前台业务',
-    menuIcon: 'fa-envelope-o',
+    menuIcon: 'fa-laptop',
     menuName: 'menu1',
     menu: [{
         menuTitle: '咨询',
-        menuIcon: 'fa-envelope-o',
-        menuName: 'menu1',
-        menu: [
-            { menuTitle: '新建咨询', menuIcon: 'fa-envelope-o', to: '/consult' },
-            { menuTitle: '咨询记录', menuIcon: 'fa-envelope-o', to: '/consult_search' },
-        ]
-    }, {
-        menuTitle: '报名',
-        menuIcon: 'fa-envelope-o',
-        to: '/student'
-    }]
-}, {
-    menuTitle: '教务教学',
-    menuIcon: 'fa-envelope-o',
-    menuName: 'menu1',
-    menu: [{
-        menuTitle: '课程设置',
-        menuIcon: 'fa-envelope-o',
+        menuIcon: 'fa-phone-square',
         to: '/coursemanage'
     }, {
-        menuTitle: '老师管理',
-        menuIcon: 'fa-envelope-o',
+        menuTitle: '报名',
+        menuIcon: 'fa-keyboard-o',
+        to: '/techermanage'
+    }, {
+        menuTitle: '签到',
+        menuIcon: 'fa-pencil-square-o',
         to: '/techermanage'
     }]
 }, {
-    menuTitle: '菜单2',
-    menuIcon: 'fa-envelope-o',
+    menuTitle: '教务教学',
+    menuIcon: 'fa-clipboard',
     menuName: 'menu1',
-    menu: [
-        { menuTitle: '菜单11', menuIcon: 'fa-envelope-o' },
-        { menuTitle: '菜单12', menuIcon: 'fa-envelope-o' },
-        { menuTitle: '菜单13', menuIcon: 'fa-envelope-o' }, {
-            menuTitle: '菜单14',
-            menuIcon: 'fa-envelope-o',
-            menuName: 'menu1',
-            menu: [
-                { menuTitle: '菜单141', menuIcon: 'fa-envelope-o' },
-                { menuTitle: '菜单142', menuIcon: 'fa-envelope-o' },
-                { menuTitle: '菜单143', menuIcon: 'fa-envelope-o' },
-                { menuTitle: '菜单144', menuIcon: 'fa-envelope-o' },
-                { menuTitle: '菜单145', menuIcon: 'fa-envelope-o' },
-            ]
-        }, {
-            menuTitle: '菜单15',
-            menuIcon: 'fa-envelope-o',
-            menuName: 'menu1',
-            menu: [
-                { menuTitle: '菜单151', menuIcon: 'fa-envelope-o' },
-                { menuTitle: '菜单152', menuIcon: 'fa-envelope-o' },
-                { menuTitle: '菜单153', menuIcon: 'fa-envelope-o' },
-                { menuTitle: '菜单154', menuIcon: 'fa-envelope-o' },
-                { menuTitle: '菜单155', menuIcon: 'fa-envelope-o' },
-            ]
-        }
-    ]
+    menu: [{
+        menuTitle: '课程设置',
+        menuIcon: 'fa-book',
+        to: '/coursemanage'
+    }, {
+        menuTitle: '班级管理',
+        menuIcon: 'fa-group',
+        to: '/techermanage'
+    }, {
+        menuTitle: '学员管理',
+        menuIcon: 'fa-user',
+        to: '/techermanage'
+    }, {
+        menuTitle: '教室管理',
+        menuIcon: 'fa-sitemap',
+        to: '/techermanage'
+    }, {
+        menuTitle: '老师管理',
+        menuIcon: 'fa-male',
+        to: '/techermanage'
+    }, {
+        menuTitle: '排课',
+        menuIcon: 'fa-calendar',
+        to: '/techermanage'
+    }]
+}, {
+    menuTitle: '系统设置',
+    menuIcon: 'fa-gear',
+    menuName: 'menu1',
+    menu: [{
+        menuTitle: '账号设置',
+        menuIcon: 'fa-credit-card',
+        to: '/coursemanage'
+    }]
 }];
