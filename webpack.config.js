32036ec27c953819d584af3d877d2929b1df069f
linzuxin <linzuxin@hotmail.com>
2017-04-24 17:16:16 +0800

    Invalid Host header修改

--- webpack.config.js
+++ webpack.config.js
@@ -86,6 +86,15 @@ module.exports = (options = {}) => ({
     devServer: {
         host: '0.0.0.0',
         port: 8010,
+        proxy: {
+            '/api/': {
+                target: 'http://0.0.0.0:8080',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        },
         historyApiFallback: {
             index: url.parse(options.dev ? '/assets/' : publicPath).pathname
         }
